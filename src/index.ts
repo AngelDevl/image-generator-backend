41a86c2b0fc193fef35be8bbde31702a3d1896dc
AngelDevl <noamharel02@gmail.com>
2025-04-09 12:29:53 +0300

    test5

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ app.use(
 app.use("/favicon.ico", express.static("public/images/image-gen-icon.png"));
 app.use("/app", express.static(public_dir));
 
-app.get("/ok", (req, res) => {
+app.get("/ok/ok", (req, res) => {
   const p = `https://v.melts.cc/app/images/Untitled.png`
   res.send(`
   <!DOCTYPE html>
